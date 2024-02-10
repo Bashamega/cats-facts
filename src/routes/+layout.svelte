9c6def5a2891b9f90ca3d1c2318c21d15cc110ec
Bashamega <adambashaahmednaji@gmail.com>
2024-02-10 12:58:15 +0200

    Cool facts about cats

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script>
+	import './styles.css';
+</script>
+
+<div class="app">
+
+	<main>
+		<slot />
+	</main>
+
+	<footer>
+		<p>Created by <a href="https://github.com/bashamega">Adam Basha</a></p>
+	</footer>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+	let fact = '';
+	function generate(){
+		fetch('https://catfact.ninja/fact')
+		.then(res=>res.json())
+		.then(data=>{
+			fact = data.fact;
+		})
+	}
+	generate()
+</script>
+<section>
+	<h1>Cat facts generator</h1>
+	<button on:click={generate}>Regenerate</button>
+	<p>Did you know that</p>
+	<div id="fact">{fact}</div>
+</section>
